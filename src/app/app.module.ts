336cf212f56d46c6c660096864fef32e19f29bc2
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-05-26 12:08:10 +0200

    Bootstrasp, Navbar, Footer, Body, Modelo pelicula.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent  ],
   imports: [
     BrowserModule
   ],
